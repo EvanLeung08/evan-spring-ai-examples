cd0eb2f91c980664931ccbf4f6d2daa680b8d0d9
eshare <lyh10856214@qq.com>
2025-04-21 14:01:35 +0800

    add demo for prompts and output parsers

---
+++ prompts-and-output-parsers/src/main/java/org/evan/ai/JokeResponse.java
@@ -0,0 +1,4 @@
+package org.evan.ai;
+
+public record JokeResponse(String subject, String joke) {
+}
---
+++ prompts-and-output-parsers/src/main/java/org/evan/ai/SpringAiEssentialExampleApplication.java
@@ -0,0 +1,13 @@
+package org.evan.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiEssentialExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiEssentialExampleApplication.class, args);
+    }
+
+}
